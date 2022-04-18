e4859e51f460c281a9a9a84b8f529d2677df3f89
Sam Pibworth <sam.pibworth@lumilinks.com>
2022-04-18 15:10:54 +0100

    parse response

--- grid.py
+++ grid.py
@@ -4,11 +4,15 @@ from response_parser import UserInput
 class Grid:
     def __init__(self, grid_size=4) -> None:
         self.grid = [[Tile() for _ in range(grid_size)] for _ in range(grid_size)]
-        self.run()
 
     def __repr__(self) -> str:
         return '\n'.join([' '.join([t.disp(4) for t in l]) for l in self.grid])
 
+    def swipe():
+        pass
+    
     def run(self):
-        resp = UserInput.get_reponse()
-        print(resp)
+        while True:
+            print(self)
+            resp = UserInput.get_response()
+            
